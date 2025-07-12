d6e326437722c9d806d051d0359aa20f8f4523be
cbchola <cholacb88@gmail.com>
2025-07-12 17:57:27 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// sw.js
 const CACHE_NAME = 'cbchola-cache-v1';
 const urlsToCache = [
   './',
@@ -24,39 +23,3 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-
-// Anggota tambahan dan motivasi
-self.daftarAnggota = [
-  "GUFRON", "IMAN", "ARDI", "FHAISAL", "RIKI", "BINTANG", "ILZAM", "DEVI", "ROZAK", "BAGONG",
-  "JOKO", "FIRDAUS", "NYOT", "SAHRONI", "ERIK", "BRED", "PETOK", "ANTOK", "ABBY", "WILDAN", "AKBAR",
-  "GALIH", "YOGI", "RAKA", "FAJAR", "NIKO", "ZUL", "RONALD", "TOMMY", "IRFAN"
-];
-
-self.pesanMotivasi = [
-  "Yuk, bagi yang belum menyetor kas, mari lengkapi kewajiban kita bersama. Sedikit dari kita, besar untuk kebersamaan!",
-  "Kas bukan sekadar iuran, tapi wujud komitmen dan kebersamaan keluarga CB CHOLA. Saling jaga, saling bantu, saling kuat!",
-  "CB CHOLA bukan hanya nama, tapi semangat satu hati. Mari terus tumbuh, berdaya, dan solid!",
-  "Yang belum sempat bayar bulan ini, yuk semangat! Kita semua pernah sibuk, tapi bareng-bareng kita bisa saling kuatkan.",
-  "Kita kuat karena saling melengkapi. Yuk, yang belum sempat bayar, mari lanjutkan semangat kebersamaan kita!",
-  "Iuran kas bukan beban, tapi bentuk dukungan untuk kebaikan bersama. Semangat terus keluarga CB CHOLA!",
-  "Sedikit dari masing-masing kita, besar manfaatnya untuk semua. Kasih support yuk, biar makin solid!",
-  "Bersama kita bisa lebih baik. Jangan biarkan satu pun tertinggal. Ayo lengkapi iuran kas bulan ini!",
-  "CB CHOLA bukan sekadar nama, tapi komitmen. Yuk jaga kekompakan, satukan langkah!",
-  "Kebersamaan dimulai dari hal kecil. Kas kita, semangat kita, kekuatan kita!",
-  "Yuk kita saling mengingatkan. Bukan karena lupa, tapi karena peduli. Waktunya setor kas!",
-  "Jangan tunggu nanti. Sedikit hari ini untuk kekuatan besar esok hari. Setor kas yuk!"
-];
-
-// Placeholder untuk aksi edit & hapus (client side only)
-self.actions = {
-  editKas: function (id, updatedData) {
-    // Firestore update example:
-    // return updateDoc(doc(db, "kas", id), updatedData);
-    console.log("Edit Kas:", id, updatedData);
-  },
-  hapusKas: function (id) {
-    // Firestore delete example:
-    // return deleteDoc(doc(db, "kas", id));
-    console.log("Hapus Kas:", id);
-  }
-};
